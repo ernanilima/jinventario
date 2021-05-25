7b5a3566416a0e9741a6ddbc35158c871b8b17ff
ernanilima <ernani.tecc@gmail.com>
2021-05-25 16:39:53 -0300

    Criado e testado telas login, cadastrar e esqueceu a senha

---
+++ app/src/main/java/br/com/ernanilima/jinventario/view/CadastrarActivity.java
@@ -0,0 +1,23 @@
+package br.com.ernanilima.jinventario.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import br.com.ernanilima.jinventario.R;
+
+public class CadastrarActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // inicia o xml
+        return inflater.inflate(R.layout.fragment_login_cadastrar, container, false);
+    }
+
+}
