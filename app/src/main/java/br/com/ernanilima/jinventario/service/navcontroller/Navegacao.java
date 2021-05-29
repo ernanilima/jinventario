df595d5ac5a9610018a28116fb2d8e577b122150
ernanilima <ernani.tecc@gmail.com>
2021-05-29 15:25:35 -0300

    Criado activity principal

--- app/src/main/java/br/com/ernanilima/jinventario/service/navcontroller/Navegacao.java
+++ app/src/main/java/br/com/ernanilima/jinventario/service/navcontroller/Navegacao.java
@@ -1,10 +1,14 @@
 package br.com.ernanilima.jinventario.service.navcontroller;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import br.com.ernanilima.jinventario.AppActivity;
 import br.com.ernanilima.jinventario.R;
 
 /** Classe criada para agrupar as navegacoes */
@@ -31,5 +35,14 @@ public class Navegacao {
     public static void abrirTelaEsqueceuSenha(View view) {
         navController = Navigation.findNavController(view);
         navController.navigate(R.id.telaEsqueceuSenha);
+    }
+
+    /** Abre tela de activity principal
+     * @param context Context - context(tela) que esta solicitando */
+    public static void abrirTelaActivityPrincipal(Context context) {
+        Activity activity = (Activity)context;
+        Intent intent = new Intent(activity, AppActivity.class);
+        activity.startActivity(intent);
+        activity.finish();
     }
 }
