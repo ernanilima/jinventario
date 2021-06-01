72d6c8412483db03aa686ee399cae6b33d89b63c
ernanilima <ernani.tecc@gmail.com>
2021-05-31 22:10:32 -0300

    Criado inicio de app, content, navegacao, atualizado appactivity

--- app/src/main/java/br/com/ernanilima/jinventario/AppActivity.java
+++ app/src/main/java/br/com/ernanilima/jinventario/AppActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -27,12 +30,25 @@ public class AppActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_Layout);
         navigationView = findViewById(R.id.nav_view);
 
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.btn_menu_nav_inicio, R.id.btn_menu_nav_novacoletagem)
+        // informar os IDs dos itens de menus associados ao drawer Layout.
+        // ao criar a navegacao, os IDs deves ser os mesmos IDs abaixo.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.btn_menu_nav_inicio, R.id.btn_menu_nav_novacoletagem)
                 .setOpenableLayout(drawerLayout)
                 .build();
 
+        NavController navController = Navigation.findNavController(this, R.id.fragment_app);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        // botao nao tem fragment para navegacao
         navigationView.getMenu().findItem(R.id.btn_menu_nav_sair).setOnMenuItemClickListener(item -> sair());
+    }
+
+    @Override
+    /** Usado para abrir drawer Layout ao pressionar o botao no toolbar */
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.fragment_app);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
 
     private boolean sair() {
