06329449a0f3e1ce6fbf3821894683e1d85d0c8a
ernanilima <ernani.tecc@gmail.com>
2021-11-17 01:21:18 -0300

    Criado metodo para ocultar o teclado, atualizado snackbar

--- app/src/main/java/br/com/ernanilima/jinventario/extension/common/DeviceHelper.kt
+++ app/src/main/java/br/com/ernanilima/jinventario/extension/common/DeviceHelper.kt
@@ -1,15 +1,23 @@
 package br.com.ernanilima.jinventario.extension.common
 
+import android.app.Activity
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.fragment.app.Fragment
+
 
 object DeviceHelper {
 
-    /** Valida a conexao com a internet
+    /**
+     * Valida a conexao com a internet
      * @param context Context
-     * @return boolean - true se tiver internet */
+     * @return boolean - true se tiver internet
+     */
     fun isInternet(context: Context?): Boolean {
         val connection = context!!.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -30,4 +38,12 @@ object DeviceHelper {
         }
     }
 
+    /**
+     * Oculta o teclado
+     * @param context Context
+     */
+    fun hideKeyboard(context: Context) {
+        val imm: InputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow((context as Activity).window.decorView.rootView.windowToken, 0)
+    }
 }
--- app/src/main/java/br/com/ernanilima/jinventario/extension/common/snackbar/SnackbarCustom.kt
+++ app/src/main/java/br/com/ernanilima/jinventario/extension/common/snackbar/SnackbarCustom.kt
@@ -1,6 +1,5 @@
 package br.com.ernanilima.jinventario.extension.common.snackbar;
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.view.LayoutInflater;
 import androidx.core.content.ContextCompat
@@ -9,11 +8,12 @@ import androidx.fragment.app.Fragment
 import br.com.ernanilima.jinventario.R;
 import br.com.ernanilima.jinventario.databinding.SnackbarCustomBinding
 import com.google.android.material.snackbar.Snackbar
+import br.com.ernanilima.jinventario.extension.common.DeviceHelper
 
 object SnackbarCustom {
 
     /**
-     * @param context Context - onde sera exibido
+     * @param fragment Fragment - onde sera exibido
      * @param message String - mensagem para exibir
      */
     fun success(fragment: Fragment, message: String) {
@@ -21,7 +21,7 @@ object SnackbarCustom {
     }
 
     /**
-     * @param context Context - onde sera exibido
+     * @param fragment Fragment - onde sera exibido
      * @param message String - mensagem para exibir
      */
     fun warning(fragment: Fragment, message: String) {
@@ -33,18 +33,25 @@ object SnackbarCustom {
     //}
 
     /**
-     * @param context Context - onde sera exibido
+     * @param fragment Fragment - onde sera exibido
      * @param type Int - background
      * @param message String - mensagem para exibir
      */
-    @SuppressLint("ShowToast")
     private fun base(fragment: Fragment, type: Int, message: String) {
+        // oculta o teclado
+        DeviceHelper.hideKeyboard(fragment.requireContext())
+
+        // busca e atribui os valores no snackbar personalizado
         val layoutInflater: LayoutInflater = LayoutInflater.from(fragment.requireContext())
         val binding: SnackbarCustomBinding = SnackbarCustomBinding.inflate(layoutInflater)
         binding.toastRoot.background = ContextCompat.getDrawable(fragment.requireContext(), type)
         binding.txtMessage.text = message
+
+        // constroi o snackbar
         val snackbar: Snackbar = Snackbar.make(fragment.requireView(), "", Snackbar.LENGTH_LONG)
         snackbar.view.setBackgroundColor(Color.TRANSPARENT)
+
+        // constroi o layout do snackbar, adiciona o snackbar personalizado e exibe
         val snackbarLayout: Snackbar.SnackbarLayout = snackbar.view as Snackbar.SnackbarLayout
         snackbarLayout.addView(binding.root)
         snackbar.show()
