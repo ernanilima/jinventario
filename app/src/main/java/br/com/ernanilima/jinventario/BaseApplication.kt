42faafde118413cbd44a77c9a5121e0847a88049
ernanilima <ernani.tecc@gmail.com>
2022-03-01 00:03:50 -0300

    GreenDao apenas no dagger hilt

--- app/src/main/java/br/com/ernanilima/jinventario/BaseApplication.kt
+++ app/src/main/java/br/com/ernanilima/jinventario/BaseApplication.kt
@@ -1,31 +1,12 @@
 package br.com.ernanilima.jinventario
 
 import android.app.Application
-import br.com.ernanilima.jinventario.repository.orm.DaoMaster
-import br.com.ernanilima.jinventario.repository.orm.DaoSession
 import dagger.hilt.android.HiltAndroidApp
-import org.greenrobot.greendao.database.Database
 
-/** Classe que inicia o dagger hilt.
+/**
+ * Classe que inicia o dagger hilt.
  * @see [link](https://developer.android.com/training/dependency-injection/hilt-android?hl=pt-br#application-class)
- * @see [link](https://medium.com/androiddevelopers/hilt-and-dagger-annotations-cheat-sheet-9adea070e495) */
+ * @see [link](https://medium.com/androiddevelopers/hilt-and-dagger-annotations-cheat-sheet-9adea070e495)
+ */
 @HiltAndroidApp
-class BaseApplication: Application() {
-    // IMPORTANTE: DaoMaster, DaoSession e DAOs sao criados automaticamente
-    // ao executar o projeto. Isso esta descriminado no proprio github.
-    // https://github.com/greenrobot/greenDAO
-    // !!TEMPORARIO!!, O GREENDAO SERA CRIADO EM UM MODULO
-
-    private var daoSession: DaoSession? = null
-
-    override fun onCreate() {
-        super.onCreate()
-        val helper: DaoMaster.DevOpenHelper = DaoMaster.DevOpenHelper(this, "jinventario-db")
-        val db: Database = helper.getWritableDb()
-        daoSession = DaoMaster(db).newSession()
-    }
-
-    fun getSessao(): DaoSession? {
-        return daoSession
-    }
-}
+class BaseApplication: Application() { }
