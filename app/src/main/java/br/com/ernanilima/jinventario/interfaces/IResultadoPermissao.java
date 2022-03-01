cfb3237df34d841a5f9cc2514432411f809e6328
ernanilima <ernani.tecc@gmail.com>
2022-02-28 23:53:16 -0300

    Removido classes que nao sao mais utilizadas

--- app/src/main/java/br/com/ernanilima/jinventario/interfaces/IResultadoPermissao.java
+++
@@ -1,7 +0,0 @@
-package br.com.ernanilima.jinventario.interfaces;
-
-public interface IResultadoPermissao {
-
-    /** @param b boolean - permissao ativada ou nao */
-    void setResultadoPermissao(boolean b);
-}
